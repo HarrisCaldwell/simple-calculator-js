b029364e03ce3b852f95f885d50e89c8cf42ceb6
Harris Caldwell <harriscaldwell@Harriss-iMac.attlocal.net>
2021-10-13 20:56:22 -0500

    initializing html elements

---
+++ simple-calculator.js
@@ -0,0 +1,6 @@
+let num1 = document.querySelector("#num-1") 
+let num2 = document.querySelector("#num-2")
+
+let operators = document.getElementsByClassName('operator')
+let submit = document.querySelector("#submit")
+let output = document.querySelector("#output")
